5566aac6a364bdaff286b8022080021b667b59a9
Khiroya, Rishi <rk1121@ic.ac.uk>
2024-02-05 17:29:12 +0000

    build: Port project from main repo

---
+++ face_detection.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np 
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+
+class FaceDetector():
+    def __init__(self):
+        self.face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    def detect_bounding_box(self, frame):
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.face_classifier.detectMultiScale(gray_frame, scaleFactor = 1.1, minNeighbors = 15, minSize=(50, 50))
+        return faces
+
+    def detect_face(self, video):
+        vidObj = cv2.VideoCapture(video)
+        fps = vidObj.get(cv2.CAP_PROP_FPS)
+        curr_frame = 0
+        boxes = []
+        while True:
+            success, video_frame = vidObj.read()
+            if not success:
+                break
+            
+            bounding_box = self.detect_bounding_box(video_frame)
+            if curr_frame == 0:
+                boxes = bounding_box
+            elif len(bounding_box) != 0:
+                boxes = np.concatenate((boxes, bounding_box), axis=0)
+                
+            curr_frame += fps
+            vidObj.set(cv2.CAP_PROP_POS_FRAMES, curr_frame)
+                
+        return boxes
+
+if __name__ == "__main__":
+    face_detector = FaceDetector()
+    
+    bounding_boxes = face_detector.detect_face("./video.mp4")
+    
+    print(bounding_boxes[0].shape)
+    
---
+++ slide_timestamping.py
@@ -0,0 +1,61 @@
+import itertools
+
+import numpy as np
+import cv2
+
+from face_detection import FaceDetector
+import SliTraNet.slide_detection_2d as SliTraNet
+
+# Calculate the x coordinates that have been cropped to not include x1 and x2
+def get_cropped_x_coords(x0, x1, frame_x0=0, frame_x1=852):
+    x0_difference = abs(x0 - frame_x0)
+    x1_difference = abs(x1 - frame_x1)
+    
+    padding = 100
+    if x0_difference < x1_difference:
+        return x1 + padding, frame_x1
+    else:
+        return frame_x0, x0 - padding
+
+# Calulate the bounding box that excludes the face overlay
+def get_bounding_box(video_path):
+    face_detector = FaceDetector()
+    bounding_boxes = face_detector.detect_face(video_path)
+    
+    # Only interested in the x coordinates of the bounding box (delete second and last column)
+    x_coords = np.delete(bounding_boxes, np.s_[1::2], axis=1)
+    m_x, m_w = np.median(x_coords, axis=0)
+    
+    cap = cv2.VideoCapture(video_path)
+    x0, x1 = get_cropped_x_coords(m_x, m_x + m_w, frame_x1=cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    return np.array([x0, 0, x1, cap.get(cv2.CAP_PROP_FRAME_HEIGHT)])
+
+# Identifies the initial timestamp of each slide, taken from a video
+def get_initial_slide_timestamps(video_path):
+    # Get bounding box of the video with the face overlay cropped out
+    bounding_box = get_bounding_box(video_path)
+    
+    # Attempt to find the initial slide transitions using the neural network
+    try:
+        slide_transitions = SliTraNet.test_resnet2d(video_path, bounding_box)
+    except:
+        return False, None
+    
+    # Find the fps of the video
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    
+    # Map the dictionary values to be the start and end timestamp of the slide in seconds
+    slide_transitions.pop(-1, None)
+    for slide_no, (start, end) in slide_transitions.items():
+        slide_transitions[slide_no] = (start / fps, end / fps)
+    
+    return True, slide_transitions
+
+if __name__ == "__main__":
+    result, slide_timestamps = get_initial_slide_timestamps("./static/iml_video.mp4")
+    
+    if result:
+        print(slide_timestamps)
+    else:
+        print("Fetching slide timestamps failed")
