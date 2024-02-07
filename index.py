16920d68de75d165ba2682051dbd9ef3c9b3a2e1
Gabriel de Olim Gaul <gabdeolimgaul@outlook.com>
2024-02-07 02:10:48 +0000

    feat: Add hug API call for fetching slide timestamps

--- index.py
+++ index.py
@@ -1,7 +1,14 @@
 import hug
 
-@hug.get('/hello')
-def hello():
-    return "Hello, World!"
+from slide_timestamping import *
+
+@hug.get('/get_timestamps')
+def get_timestamps(video_path, slides_json):
+    result, slide_timestamps = get_slide_timestamps(video_path, slides_json)
+    
+    if result:
+        return slide_timestamps
+    else:
+        return "Fetching slide timestamps failed"
 
 print("https://localhost:8000")
