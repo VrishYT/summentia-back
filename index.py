5c9129e5adcb0f38056d1d2c3f6d14eb01f1acf3
Dami <dme21@ic.ac.uk>
2024-02-08 20:53:12 +0000

    feat: Add response middleware to index.py to solve CORS-related error

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
 import hug
+from falcon import HTTP_200
 import json
-
 from slide_timestamping import *
+
+api = hug.API(__name__)
+
+@hug.response_middleware()
+def process_response(request, response, resource):
+    response.set_header('Access-Control-Allow-Origin', '*')
+    response.set_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    response.set_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
 @hug.post('/get_timestamps')
 def get_timestamps(video_path, slides_json):
@@ -11,5 +19,5 @@ def get_timestamps(video_path, slides_json):
         return json.dumps(slide_timestamps)
     else:
         return "Fetching slide timestamps failed"
-
+    
 print("https://localhost:8000")
