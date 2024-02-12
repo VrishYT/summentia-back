188d566e0019338553173edf463b784888089d14
Dami <dme21@ic.ac.uk>
2024-02-12 14:16:50 +0000

    feat: Send data in map instead of pure string

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ def get_timestamps(video_path, slides_json):
     result, slide_timestamps = get_slide_timestamps(video_path, slides_json)
     
     if result:
-        return json.dumps(slide_timestamps)
+        return {'data': json.dumps(slide_timestamps)}
     else:
         return "Fetching slide timestamps failed"
     
