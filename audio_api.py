23555747236137272a99705cafff1954d52061be
Dami <dme21@ic.ac.uk>
2024-02-19 16:08:57 +0000

    feat: Add audio_extractor to API calls

--- audio_api.py
+++ audio_api.py
@@ -1,11 +1,19 @@
 import hug
 from falcon import HTTP_200
 import json
+from audio_extractor import extract_audio
+
+@hug.exception(Exception)
+def exception_handler(exception):
+  # print("\n\nEXCEPTION\n\n")
+  # print(exception)
+  # return {'error': exception }
+  raise exception
 
 @hug.get('/hello')
 def test_hello():
   return {'data': "HELLO WORLD"}
   
 @hug.post('/extract')
-def extract_audio(video_path):
-  pass
+def extract(video_path, timestamps):
+  extract_audio(video_path, timestamps)
--- audio_extractor.py
+++ audio_extractor.py
@@ -1,7 +1,21 @@
 import ffmpeg
 
 def extract_audio(video_path, timestamps):
-    ffmpeg.input(video_path).output('video_%03d.mp3', audio_bitrate='96k', map='0', f='segment', segment_start_number='1', segment_frames='2000,3000,4000').run()
+    # timestamps string should be passed as a comma separated list of frames, no spaces
+    try:
+        # ffmpeg.input(video_path).output('video_%03d.mp3', audio_bitrate='96k', map='0', f='segment', segment_start_number='1', segment_frames=timestamps).run()
+        input = ffmpeg.input(video_path)
+        out = input.output('video_%03d.mp3', audio_bitrate='96k', map='0', f='segment', segment_start_number='1', segment_frames=timestamps)
+        out.run(capture_stdout = True, capture_stderr = True)
+    except ffmpeg.Error as err:
+        # print()
+        # print('stdout:', err.stdout.decode('utf8'))
+        # print()
+        # print('stderr:', err.stderr.decode('utf8'))
+        # print('\n\nEND OF STUFF\n\n')
+        raise err
+        
+    
 
 if __name__ == "__main__":
-    extract_audio('video.mp4', {})
+    extract_audio('video.mp4', '2000,3000,4000')
