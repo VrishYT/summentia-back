c0932fc239a5a08b50d25157e7b9c022c1ac5ed8
Dami <dme21@ic.ac.uk>
2024-02-19 20:07:18 +0000

    feat: Set up transcriber using faster_whisper package

--- audio_api.py
+++ audio_api.py
@@ -2,6 +2,7 @@ import hug
 from falcon import HTTP_200
 import json
 from audio_extractor import extract_audio
+from audio_transcriber import get_transcripts_from_segments
 
 @hug.exception(Exception)
 def exception_handler(exception):
@@ -17,3 +18,18 @@ def test_hello():
 @hug.post('/extract')
 def extract(video_path, timestamps):
   extract_audio(video_path, timestamps)
+
+@hug.post('/transcribe')
+def transcribe(file_count):
+  transcripts = get_transcripts_from_segments(int(file_count))
+  return {'data': json.dumps(transcripts)}
+
+@hug.post('/convert')
+def convert_video_to_audio():
+  try:
+    extract_audio("video.mp4", "2000,3000,4000")
+    transcripts = transcribe(4)
+    return {'data': json.dumps(transcripts)}
+    
+  finally:
+    pass
