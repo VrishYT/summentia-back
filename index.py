b04a5e0ba8476ae122ead304985b12ca0f691892
Rishi Khiroya <rk1121@ic.ac.uk>
2024-03-06 10:23:01 +0000

    feat: Finish implementing process_genslides

--- index.py
+++ index.py
@@ -85,12 +85,12 @@ def process_slides(project_folder, response):
                 "squashed": squashed_info.get('squashed')
             }
 
-    return squashed_json, slides_data
+    return {"slides": squashed_json, "data": slides_data}
 
 
 @hug.post('/process_noslides')
 def process_noslides(project_folder):
-    print(f"Processing {project_folder}...")
+    print(f"Processing {project_folder}.")
     audio_path = extract_single_audio(project_folder)
     print(f"Audio extracted.")
     transcript = transcribe(audio_path)
@@ -100,10 +100,51 @@ def process_noslides(project_folder):
 
 @hug.post('/process_genslides')
 def process_genslides(project_folder, response):
-    success, transitions, frames = get_slide_timestamps(os.path.join(project_folder, "video.mp4"), project_folder)
+    video_path = os.path.join(project_folder, "video.mp4")
+    success, transitions, frames = get_slide_timestamps(video_path, project_folder)
     if not success:
         response.status = falcon.get_http_status(400)
         return
 
     slides_json, timestamps = generate_slides(project_folder, transitions, frames)
-    return slides_json, timestamps
+
+    timestamp_to_slide = {}
+
+    num_slides = slides_json.get("num_slides")
+
+    for slide_no in range(num_slides):
+        for segment in timestamps[slide_no]:
+            end_frame = segment["end"]
+            timestamp_to_slide[str(end_frame)] = int(slide_no)
+
+    sorted_dict = OrderedDict(sorted(timestamp_to_slide.items(), key=lambda x: int(x[0])))
+    keys = sorted_dict.keys()
+    frame_options = ','.join(keys)
+
+    # use Whisper to transcribe audio
+    extract_audio(project_folder, video_path, frame_options)
+
+    transcripts = get_transcripts_from_segments(project_folder, len(keys), 0)
+    slide_nos = list(timestamps.values())
+
+    slides_data = {}
+    slides = slides_json.get("slides")
+
+    for i, transcript in enumerate(transcripts):
+        slide_no = slide_nos[i]
+        if slide_no in slides_data:
+            data = slides_data[slide_no]
+            print(data)
+            data["transcript"].append(transcript)
+            print(data["transcript"])
+            slides_data[slide_no] = data
+        else:
+            squashed_info = slides[slide_no]
+            slides_data[slide_no] = {
+                "slide": squashed_info.get('path'),
+                "transcript": [transcript],
+                "summary": [],
+                "squashed": squashed_info.get('squashed')
+            }
+
+    return {"slides": slides_json, "data": slides_data}
