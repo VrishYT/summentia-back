4bcf4879cec1141d3fe85cfc3ab9e4f63f658f1c
Gabriel de Olim Gaul <gabdeolimgaul@outlook.com>
2024-03-11 17:01:59 +0000

    fix: Fix extract_audio to not use subtitle stream for videos

--- audio_extractor.py
+++ audio_extractor.py
@@ -11,14 +11,14 @@ def extract_audio(project_folder, video_path, timestamps, out_index=0):
         audio_folder = os.path.join(project_folder, "audio/")
         if not (os.path.exists(audio_folder) and os.path.isdir(audio_folder)):
             os.mkdir(audio_folder)
-        out = input.output(os.path.join(audio_folder, "%03d.mp3"), audio_bitrate='96k', map='0', f='segment',
+        out = input.output(os.path.join(audio_folder, "%03d.mp3"), audio_bitrate='96k', f='segment',
                            segment_start_number=str(out_index), segment_frames=timestamps)
         print("Segmenting audio")
         out.run(capture_stdout=True, capture_stderr=True)
         print("Segmented audio successfully")
 
     except ffmpeg.Error as err:
-        print("Error segmenting audio")
+        print(f"Error segmenting audio: {err.stderr}")
         raise err
 
 
@@ -32,6 +32,7 @@ def extract_single_audio(project_folder):
         input = ffmpeg.input(os.path.join(project_folder, "video.mp4"))
         out = input.output(mp3_path, audio_bitrate='96k')
         out.run(capture_stdout=True, capture_stderr=True)
+        print(mp3_path)
         return mp3_path
     except ffmpeg.Error as err:
         raise err
